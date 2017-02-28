26bc0da79c12832f4800ea41e267e46d3f63f1d2
MsZlets <you@example.com>
2017-02-28 23:33:46 +0300

    add bootsrtrap

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-vadidates :title, presence: true, length: {minimum: 3, Maximum: 50}
-vadidates :description, presence: true, length: {minimum: 10, Maximum: 300}
+validates :title, presence: true, length: {minimum: 3, Maximum: 50}
+validates :description, presence: true, length: {minimum: 10, Maximum: 300}
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
 root to: "page#home"
 get "about", to: "page#about"
+resources :articles
 end
