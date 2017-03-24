488f86e5af927512ff3a2517ea24691fa1895b41
MsZlets <you@example.com>
2017-03-24 19:36:05 +0300

    add Admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323191723) do
+ActiveRecord::Schema.define(version: 20170324161120) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -23,9 +23,10 @@ ActiveRecord::Schema.define(version: 20170323191723) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
