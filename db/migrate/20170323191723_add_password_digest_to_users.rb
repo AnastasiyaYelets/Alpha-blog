a27697ebda1daa88f7b74ec42613964ab99b1981
MsZlets <you@example.com>
2017-03-26 13:55:11 +0300

    fix password

--- db/migrate/20170323191723_add_password_digest_to_users.rb
+++ db/migrate/20170323191723_add_password_digest_to_users.rb
@@ -1,5 +1,5 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :password_digest, :stringrake:
+    add_column :users, :password_digest, :stringrake
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324161120) do
+ActiveRecord::Schema.define(version: 20170326104942) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -20,12 +20,18 @@ ActiveRecord::Schema.define(version: 20170324161120) do
     t.integer  "user_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
-    t.string   "password_digest"
+    t.         "password_digest"
     t.boolean  "admin",           default: false
   end
 
